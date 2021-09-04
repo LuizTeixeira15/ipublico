620fa5dc73123310370ed7767baeede14b92adf8
LuizTeixeira15 <luiz_ecia@hotmail.com>
2021-09-04 20:13:08 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<style type="text/css">
+		body{
+			margin: 5%;
+			padding: 15%
+		}
+	</style>
+	<title>Hora</title>
+</head>
+<body>
+
+	<button class="btn">Hora</button>
+
+</body>
+</html>
